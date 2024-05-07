5133349966d1948af7777b12a77d64ea538cb28c
Oskarowski <oscar.kacprzak@gmail.com>
2024-05-07 10:58:10 +0200

    add private constructor

--- Dao/src/main/java/sudoku/dao/factories/SudokuBoardDaoFactory.java
+++ Dao/src/main/java/sudoku/dao/factories/SudokuBoardDaoFactory.java
@@ -7,6 +7,13 @@ import sudoku.model.models.SudokuBoard;
 public class SudokuBoardDaoFactory {
 
     /**
+     * The `SudokuBoardDaoFactory` class is a factory class that provides methods for creating instances of `FileSudokuBoardDao`.
+     * It shouldn't be possible to create an instance of this class.
+     */
+    private SudokuBoardDaoFactory() {
+    }
+
+    /**
      * Factory method to create Dao class for SudokuBoard.
      *
      * @param dirPath path to directory where files are stored
