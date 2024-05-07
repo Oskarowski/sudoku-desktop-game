d337979d2774fec905eebe37aa3f51b105657a8f
247027 Wojciech Kapica <247027@edu.p.lodz.pl>
2024-05-07 14:13:24 +0200

    SudokuFieldTest created to test compareTo()

---
+++ Model/src/test/java/models/SudokuFieldTest.java
@@ -0,0 +1,25 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import sudoku.model.models.SudokuField;
+
+public class SudokuFieldTest {
+
+    @Test
+    void testCompareTo() {
+        SudokuField field1 = new SudokuField(1);
+        SudokuField field2 = new SudokuField(2);
+        SudokuField field3 = new SudokuField(3);
+
+        assertTrue(field1.compareTo(field2) < 0);
+        assertTrue(field2.compareTo(field3) < 0);
+        assertTrue(field1.compareTo(field3) < 0);
+
+        assertTrue(field2.compareTo(field1) > 0);
+        assertTrue(field3.compareTo(field2) > 0);
+        assertTrue(field3.compareTo(field1) > 0);
+    }
+}
