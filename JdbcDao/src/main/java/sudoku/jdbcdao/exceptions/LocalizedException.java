655a86e7cefe692e20ddffa0bb2ca9a7bcab99e0
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2024-06-13 15:14:37 +0200

    add JavaDoc to class, with proper local bundle getter

--- JdbcDao/src/main/java/sudoku/jdbcdao/exceptions/LocalizedException.java
+++ JdbcDao/src/main/java/sudoku/jdbcdao/exceptions/LocalizedException.java
@@ -1,17 +1,42 @@
 package sudoku.jdbcdao.exceptions;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * Represents a localized exception base class that can be thrown by the JdbcDao
+ * module.
+ * This exception is used to provide localized error messages to the user.
+ */
 public class LocalizedException extends Exception {
+    /**
+     * Retrieves the localized message for the given message key.
+     *
+     * @param messageKey the key of the message to retrieve
+     * @return the localized message associated with the key
+     */
     private static String getLocalizedMessage(String messageKey) {
-        ResourceBundle bundle = ResourceBundle.getBundle("JdbcDaoExceptions"); // remember that resource bundles must be
+        ResourceBundle bundle = ResourceBundle.getBundle("JdbcDaoExceptions", Locale.getDefault());
         return bundle.getString(messageKey);
     }
 
+    /**
+     * Constructs a new LocalizedException with the specified messenger.
+     *
+     * @param messenger the JdbcDaoMessageKey representing the localized message
+     *                  key.
+     */
     public LocalizedException(JdbcDaoMessageKey messenger) {
         super(getLocalizedMessage(messenger.getKey()));
     }
 
+    /**
+     * Constructs a new LocalizedException with the specified messenger and cause.
+     *
+     * @param messenger the JdbcDaoMessageKey representing the localized message
+     *                  key.
+     * @param cause     the Throwable that caused this exception to be thrown.
+     */
     public LocalizedException(JdbcDaoMessageKey messenger, Throwable cause) {
         super(getLocalizedMessage(messenger.getKey()), cause);
     }
